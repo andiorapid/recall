c8e2a1efbf8571a4e1aff6c152a1f77fe4089307
Andrii <andioblack24@gmail.com>
2025-11-12 23:51:06 +0200

    Feat: Add script to add 2 numbers and print result into terminal

---
+++ Scripts/PHP/NumberAddition/NumberAddition.php
@@ -0,0 +1,13 @@
+<?php
+
+$num1 = readline("Write your first number: ");
+$num2 = readline("Write your second number: ");
+
+$num1 = (int)$num1;
+$num2 = (int)$num2;
+
+$num = $num1 + $num2;
+
+echo "Reault = $num" . PHP_EOL;
+
+?>
