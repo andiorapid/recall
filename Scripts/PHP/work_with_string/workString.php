f0cabc756914267814367095f48468ccf88924a9
Andrii <andioblack24@gmail.com>
2025-11-15 21:59:17 +0200

    Feat: Add script that takes string and works with it.

---
+++ Scripts/PHP/work_with_string/workString.php
@@ -0,0 +1,9 @@
+<?php
+
+$str = readline("Write your string:");
+
+echo "String length: " . strlen($str) . PHP_EOL;
+echo "String upper: " . strtoupper($str) . PHP_EOL;
+echo "String reverse:" . strrev($str) . PHP_EOL;
+
+?>
