18ac8b5968ea0c326fd403a6f12258c8cc50b231
Andrii <andioblack24@gmail.com>
2025-11-15 21:53:01 +0200

    Feat: Add script that checks is person adult or not

---
+++ Scripts/PHP/adultOrNot/checkOnAdult.php
@@ -0,0 +1,11 @@
+<?php
+
+$age = readline("Write your age: ");
+if ($age > 18) {
+  echo "Allow. You are adult." . PHP_EOL;
+} else {
+  echo "Do not allow. You are not adult." . PHP_EOL;
+}
+
+
+?>
