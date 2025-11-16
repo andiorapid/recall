bee7bf00f660e8cbcffbc3545fce3bc7b150f504
Andrii <andioblack24@gmail.com>
2025-11-16 23:01:35 +0200

    Feat: Add python script that checks is number positive, negative or zero

---
+++ Scripts/Python/IsNumberPON/IsNumber.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+num = int(input("Write your number: "))
+
+if num > 0:
+    print("Your number is positive")
+elif num < 0:
+    print("Your number is negative")
+else:
+    print("Your number is zero!")
