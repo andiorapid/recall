2117e4c6da32cf7cf965ccb45a10a88471c4961f
Andrii <andioblack24@gmail.com>
2025-11-25 19:18:28 +0200

    Feat: Add C# program that checks number is positive or not

---
+++ Csh/IsNumperPON/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write your number: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num > 0)
+{
+    Console.WriteLine("Your number positive.");
+} 
+else if (num < 0)
+{
+    Console.WriteLine("Your number is negative.");
+}
+else 
+{
+    Console.WriteLine("Your number is zero!");
+}
