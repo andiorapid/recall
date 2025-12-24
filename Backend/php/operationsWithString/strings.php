155a0c824d48c00f4bd6fba4c7e1c48934209a69
Andrii <andioblack24@gmail.com>
2025-12-24 22:47:44 +0200

    Test: Tested string functions in php

---
+++ Backend/php/operationsWithString/strings.php
@@ -0,0 +1,10 @@
+<?php
+  $mySuperString = "Mr Big Pinut";
+  echo strlen($mySuperString);
+  echo str_replace("Mr", "Mss", $mySuperString);
+  echo substr($mySuperString, 0, 6);
+  echo strpos($mySuperString, "M");
+  echo strtoupper($mySuperString);
+  echo strtolower($mySuperString);
+  echo trim("              Hello super strings ");
+  echo strrev("123456");
