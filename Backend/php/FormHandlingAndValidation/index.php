82bddaac944e2edf647ddece60395f02b0d77b88
Andrii <andioblack24@gmail.com>
2026-01-02 14:06:02 +0200

    Feat: Add index.php that has user form

---
+++ Backend/php/FormHandlingAndValidation/index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Form Handling</title>
+  </head>
+  <body>
+<form method="post" action="process_form.php">
+      <label for="username">Username:</label>
+      <input type="text" name="username" id="username" required>
+      <label for="password">Password:</label>
+      <input type="password" name="password" id="password" required>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
